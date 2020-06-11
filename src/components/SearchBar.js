ce86c1a1820cfdbc2c5c1e6c7b55edf71f1baf72
Mallory Robertson <mallory@truss.works>
2020-06-11 13:20:40 -0700

    Started section 11

---
+++ src/components/SearchBar.js
@@ -0,0 +1,20 @@
+import React from  'react';
+
+class SearchBar extends React.Component{
+    state =  { term: ''};
+
+    render() {
+        return (
+            <div className="ui segment search-bar">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text" value={this.state.term} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
